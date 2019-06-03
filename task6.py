5fdecef521a50183cbe249d328605b2bc967b952
hps1991 <51230492+hps1991@users.noreply.github.com>
2019-06-03 20:52:55 +0530

    Update task6.py

--- task6.py
+++ task6.py
@@ -1,5 +1,5 @@
 #Type of triangle
-sides=list(input("Enter the Sides:\n").split())
+sides=input("Enter the Sides:\n").split()
 if(sides[0]==sides[1]==sides[2]):print("Equilateral")
 elif(sides[0]==sides[1] or sides[0]==sides[2] or sides[1]==sides[2]):print("Isosceles")
 else: print("Scalene")
